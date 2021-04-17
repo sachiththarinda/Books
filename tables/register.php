c5d7a5fc6d322c17150f23d83b0af2d3c45d3690
roshelrao <roshelrao1@gmail.com>
2021-04-17 19:10:55 +0530

    register changes

--- tables/register.php
+++ tables/register.php
@@ -5,17 +5,20 @@ $user_email=$_REQUEST["email"];
 $user_pno=$_REQUEST["pno"];
 $user_pass=$_REQUEST["pass"];
 
+
+
 $mysql= new mysqli ("localhost","root","","bookshop") or die (mysqli_connect_error());
 
 $sql1="insert into user (uname, email,pno,pass) values ('$user_name','$user_email','$user_pno', '$user_pass')";
 
     if(mysqli_query($mysql,$sql1)){
 
-        header("location:home.php");
+        header('Location:../home.php');
     }
 
-else{
+    else{
     echo"failed";
 }
+
 
 ?>
