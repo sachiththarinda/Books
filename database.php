0ca3ce8cd8268134571746b7ff9c4c7cac11c3a4
roshelrao <roshelrao1@gmail.com>
2021-04-17 17:04:01 +0530

    items table and orders table

--- database.php
+++ database.php
@@ -25,4 +25,40 @@ if(mysqli_select_db($mysql,"bookshop")){
         echo"user error" .mysqli_error($mysql);
     }
 }
+
+if(mysqli_select_db($mysql,"bookshop")){
+    $sql3="create table if not exists items(
+        itid int primary key auto_increment,
+        iname varchar(100) not null,
+        price int(10) not null,
+        image varchar(50) not null
+        )";
+    
+    if(mysqli_query($mysql,$sql3)){
+        echo"items succesfull";
+    }
+    else{
+        echo"items error" .mysqli_error($mysql);
+    }
+}
+
+if(mysqli_select_db($mysql,"bookshop")){
+    $sql4="create table if not exists orders(
+        uid int(11),
+        itid int(11),
+        price int(10) not null,
+        image varchar(50) not null,
+        foreign key (uid) references user(uid),
+        foreign key (itid) references items(itid)
+        )";
+    
+    if(mysqli_query($mysql,$sql4)){
+        echo"orders succesfull";
+    }
+    else{
+        echo"orders error" .mysqli_error($mysql);
+    }
+}
+
+
     
