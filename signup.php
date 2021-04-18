387064cf837227f06645ec2e137ebb5b0d516ad2
roshelrao <roshelrao1@gmail.com>
2021-04-18 11:18:28 +0530

    sign up sign in changes

--- signup.php
+++ signup.php
@@ -10,16 +10,15 @@
 
 <?php include 'navbar.php'?>
 
-<<<<<<< HEAD
+
 <form method="post" action="tables/register.php" onsubmit="return checkForm(this);" style="background-image: url(images/Harry-Potter-Book-List_BL_16-9.jpg.corpimagerendition.xxl.1400.788.png);,background-size: cover;">
-=======
-<<<<<<< HEAD
+
 <form ... onsubmit="return checkForm(this);">
 	<h1 align="center">User Registration</h1>
-=======
+
 <form method="post" action="tables/register.php" onsubmit="return checkForm(this);" style="background-image: url(Harry-Potter-Book-List_BL_16-9.jpg.corpimagerendition.xxl.1400.788.png);,background-size: cover;">
->>>>>>> c5d7a5fc6d322c17150f23d83b0af2d3c45d3690
->>>>>>> e90f9e0d4e18a13cd3a8852704e70f553a590bff
+
+
 <table  class="login"  align="center" >
 <tr><th colspan=2></th></tr>
 
